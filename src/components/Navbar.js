f3e689ef99d633d0f12a01d1b8cac9d65bdb28b3
MaxDobisz <maciejdobisz@gmail.com>
2023-01-27 17:27:15 +0000

    add navbar compoonent and add custom style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,23 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import { NavTab } from './NavTab';
+
 export const Navbar = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>Navbar</div>
-    )
+        <Box sx={{ width: '100%', bgcolor: 'transparent', fontSize: 40 }}>
+            <Tabs value={value} onChange={handleChange} centered textColor='inherit' indicatorColor='false'>
+                <NavTab label="HOME" />
+                <NavTab label="HOME" />
+                <NavTab label="PLAN BUILDER" />
+                <NavTab label="FITNEWS" />
+            </Tabs>
+        </Box>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from '@mui/material';
 import { App } from "./App";
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,8 +9,25 @@ import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const theme = createTheme({
+    palette: {
+        steelBlue1: "#457AA5ff",
+        steelBlue2: "#4E85AFff",
+        starCommandBlue: "#3676A3ff",
+        gunmetal: "#1B2932ff",
+        camel: "#AE895Cff",
+        camel2: "#B48B59ff",
+        richBlack1: "#0A161Bff",
+        richBlack2: "#020505ff",
+        richBlack3: "#060B0Eff",
+        khaki: "#BDB098ff",
+    }
+});
+
 root.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>
 );
