4846bf5206eb71a0be7466857e23be5d97832e34
MaxDobisz <maciejdobisz@gmail.com>
2023-01-30 09:09:20 +0000

    add css baseLine reset

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { AppRoutes } from "./AppRoutes";
 import background from './assets/homebg.jpg';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const App = () => {
     return (
-        <div style={{ height: '100vh', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-            <BrowserRouter>
-                <Navbar />
-                <AppRoutes />
-            </BrowserRouter>
-        </div>
+        <>
+            <CssBaseline />
+            <div style={{ height: '100vh', backgroundImage: `url(${background})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+                <BrowserRouter>
+                    <Navbar />
+                    <AppRoutes />
+                </BrowserRouter>
+            </div>
+        </>
     )
 }
