17ef8e7812fb4de1198c582ec27d20a2678ca963
MaxDobisz <maciejdobisz@gmail.com>
2023-02-04 12:42:04 +0000

    add transfer list for each day, remove moving items left

--- src/components/DaysSelector.js
+++ src/components/DaysSelector.js
@@ -1,33 +1,31 @@
-import { useState } from 'react';
 import { Box, NativeSelect, InputLabel, FormControl } from '@mui/material';
 
-export const DaysSelector = () => {
-    const [day, setDay] = useState('');
+export const DaysSelector = ({ day, setDay }) => {
 
     const handleChange = (event) => {
         setDay(event.target.value);
     };
 
     return (
-        <Box minWidth={120} backgroundColor='white' fontWeight='700' border='2px solid black' >
-            <FormControl fullWidth >
-                <InputLabel variant="standard" htmlFor="uncontrolled-native" sx={{ height: '1.5rem' }}>
+        <Box minWidth={120} backgroundColor='white' fontWeight='700' border='2px solid black' p='.5rem' >
+            <FormControl fullWidth onChange={handleChange}>
+                <InputLabel variant="standard" htmlFor="uncontrolled-native" >
                     Day of the week
                 </InputLabel>
-                <NativeSelect sx={{ height: '2.5rem', border: 'none' }}
-                    defaultValue={30}
+                <NativeSelect sx={{ fontWeight: '700' }}
+                    // defaultValue={30}
                     inputProps={{
                         name: 'day',
                         id: 'uncontrolled-native',
                     }}
                 >
-                    <option value={1}>Monday</option>
-                    <option value={2}>Tuesday</option>
-                    <option value={3}>Wednesday</option>
-                    <option value={4}>Thursday</option>
-                    <option value={5}>Friday</option>
-                    <option value={6}>Saturday</option>
-                    <option value={7}>Sunday</option>
+                    <option value={'monday'} style={{ textAlign: 'center' }}>Monday</option>
+                    <option value={'tuesday'} style={{ textAlign: 'center' }}>Tuesday</option>
+                    <option value={'wednesday'} style={{ textAlign: 'center' }}>Wednesday</option>
+                    <option value={'thursday'} style={{ textAlign: 'center' }}>Thursday</option>
+                    <option value={'friday'} style={{ textAlign: 'center' }}>Friday</option>
+                    <option value={'saturday'} style={{ textAlign: 'center' }}>Saturday</option>
+                    <option value={'sunday'} style={{ textAlign: 'center' }}>Sunday</option>
                 </NativeSelect>
             </FormControl>
         </Box>
