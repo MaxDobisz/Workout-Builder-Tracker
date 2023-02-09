f9996822b7f24f817c22654be09e80570d1be9a7
MaxDobisz <maciejdobisz@gmail.com>
2023-02-09 00:06:01 +0000

    style tracker

--- src/components/PlanAccordion.js
+++ src/components/PlanAccordion.js
@@ -5,12 +5,14 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Stack } from '@mui/system';
 import { NumberSelector } from './NumberSelector';
+import { Box } from '@mui/material';
 
 export const PlanAccordion = ({ plans, plan }) => {
-    console.log(plan.days)
     return (
-        <Stack>
-            <Stack direction='row' justifyContent='center' gap='2rem' sx={{ bgcolor: 'white' }}>
+        <Stack borderRadius='5px'>
+            <Stack direction={{ sm: 'row' }} justifyContent='center' gap={{ sm: '2rem' }} bgcolor='steelBlue2' borderRadius='5px 5px 0 0' sx={{
+                textShadow: '0px 0px 3px white',
+            }} >
                 <Typography textAlign='center' fontSize='1.5rem'> PLAN: {plans.indexOf(plan) + 1}</Typography>
                 <Typography textAlign='center' fontSize='1.5rem'> {plan.creationDate}</Typography>
                 <Typography textAlign='center' fontSize='1.5rem'> Days: {Object.keys(plan.days).length}</Typography>
@@ -18,28 +20,36 @@ export const PlanAccordion = ({ plans, plan }) => {
             <Stack>
                 {Object.keys((plan.days)).map(day => {
                     return (
-                        <Accordion disableGutters>
+                        <Accordion disableGutters square>
                             <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel1a-content"
                                 id="panel1a-header"
                             >
                                 <Typography m='0 auto'>{day.toUpperCase()}</Typography>
-
                             </AccordionSummary>
                             {
                                 plan.days[day].map(exercise => {
                                     return (
                                         <AccordionDetails>
-                                            <Stack direction='row' alignItems='center' gap='1rem' fontWeight='500'>
+                                            <Stack direction='row' justifyContent='space-between' alignItems='center' gap='1rem' fontWeight='500'>
                                                 {exercise.name.toUpperCase()}
-                                                <Stack direction='row' alignItems='center' fontWeight='400'>
-                                                    {`Sets: ${exercise.sets}`}
-                                                    <NumberSelector />
-                                                </Stack>
-                                                <Stack direction='row' alignItems='center' fontWeight='400'>
-                                                    {`Reps: ${exercise.reps}`}
-                                                    <NumberSelector />
+                                                <Stack direction={'row'} gap='1.5rem'>
+                                                    <Stack direction='row' alignItems='center' fontWeight='400'>
+                                                        <Box mr='1rem'>
+                                                            Sets:
+                                                        </Box>
+
+                                                        <NumberSelector optionsNumber={exercise.sets} />
+                                                        {` / ${exercise.sets}`}
+                                                    </Stack>
+                                                    <Stack direction='row' alignItems='center' fontWeight='400'>
+                                                        <Box mr='1rem'>
+                                                            Reps:
+                                                        </Box>
+                                                        <NumberSelector optionsNumber={exercise.reps} />
+                                                        {` / ${exercise.reps}`}
+                                                    </Stack>
                                                 </Stack>
                                             </Stack>
                                         </AccordionDetails>
--- src/components/TrackerPlans.js
+++ src/components/TrackerPlans.js
@@ -2,11 +2,9 @@
 import { Stack } from "@mui/system";
 import { PlanAccordion } from './PlanAccordion';
 
-
-
 export const TrackerPlans = ({ plans }) => {
     return (
-        <Stack gap='1rem'>
+        <Stack gap='1rem' maxHeight='85vh' maxWidth='max-content' overflow='auto' margin='0 auto' borderRadius='5px'>
             {plans.map(plan => {
                 return (
                     <PlanAccordion plans={plans} plan={plan} />
